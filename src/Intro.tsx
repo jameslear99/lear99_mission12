f79e93e60fbe9077fd9a3884e85c0f38b310f4f9
jameslear99 <jameslear99@gmail.com>
2023-03-22 21:58:21 -0600

    mission12

---
+++ src/Intro.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Intro() {
+    return(
+        <div>
+        <h1>Welcome!</h1>
+        <h2>This is the site that's all about NCAA Basketball.</h2>
+        <h3>Enjoy!</h3>
+        </div>
+        );
+}
+
+export default Intro;
